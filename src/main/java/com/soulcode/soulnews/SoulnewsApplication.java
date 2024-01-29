c853de8f3fc1c135bb53c0afbcc423e20bec808e
Carlos Aruh <carlos.aruh@gmail.com>
2024-01-29 14:46:39 -0300

    up

---
+++ src/main/java/com/soulcode/soulnews/SoulnewsApplication.java
@@ -0,0 +1,13 @@
+package com.soulcode.soulnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soulcode/soulnews/SoulnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulcode.soulnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
